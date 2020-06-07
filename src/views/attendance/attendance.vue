d9e9fc4ec27d2b728d60b7d85749dc421db7daf5
flygit <1224178565@qq.com>
2020-06-07 19:06:34 +0800

    初始化项目

---
+++ src/views/attendance/attendance.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>考勤界面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Teacher',
+  data() {
+    return {}
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/views/sicknote/note.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>请假条申请界面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Teacher',
+  data() {
+    return {}
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
