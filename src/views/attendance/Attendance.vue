a66526394233a2792668b4b7cb46db398fa2af6b
flygit <1224178565@qq.com>
2020-06-12 08:43:55 +0800

    更改文件名

---
+++ src/views/attendance/Attendance.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>考勤界面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Attendance',
+  data() {
+    return {}
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/views/sicknote/Note.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>-请假条申请界面-</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Note',
+  data() {
+    return {}
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
